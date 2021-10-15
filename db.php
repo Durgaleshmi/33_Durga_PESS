bcb7c4de4074a2324352d6859bf31b622a62eb6a
Durgaleshmi <92512984+Durgaleshmi@users.noreply.github.com>
2021-10-15 10:19:12 +0800

    Update db.php

--- db.php
+++ db.php
@@ -4,4 +4,4 @@ define('DB_USER', "root"); // db user
 define('DB_PASSWORD', ""); // db password 
 define('DB_DATABASE', "pessdb"); // database name
 define('DB_SERVER', "localhost"); // db server
-?>
+ ?>
